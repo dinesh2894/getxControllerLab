a320d9883c66f8088464eb37f178958b6ca05e48
dinesh28942894@gmail.com <sainidinesh2894@gmail.com>
2023-11-20 23:04:20 +0530

    one riverpod dependencies add

---
+++ lib/features/home/presentation/components/home_banner_component.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeBannerComponent extends StatelessWidget {
+  const HomeBannerComponent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/features/home/presentation/provider/home_provider.dart
@@ -0,0 +1,6 @@
+import 'package:riverpod/riverpod.dart';
+
+final homeProvider = Provider<String>((ref) {
+  return "Home Provider";
+});
+
