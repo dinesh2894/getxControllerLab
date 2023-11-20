5ce7a9a41f9629471dd7fd0361a6eeed036b6d35
dinesh28942894@gmail.com <sainidinesh2894@gmail.com>
2023-11-20 22:38:10 +0530

    Adding Some Color code

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,3 +1,11 @@
-class Constants{
+import 'dart:ui';
 
+class Constants {
+  static Color get red => const Color(0xFFFFFFFF);
+  static Color get darkRed => const Color(0xFFA12525);
+  static Color get lightRed => const Color(0xFFB77474);
+  static Color get lightGreen => const Color(0xFF7CCE17);
+  static Color get blue => const Color(0xFF1032DC);
+  static Color get lightWhite => const Color(0xFFFFFFFF);
+  static Color get darkWhite => const Color(0xFFFFFFFF);
 }
